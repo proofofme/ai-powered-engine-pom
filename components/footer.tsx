56bb84b46b0c164a639595ab908d7f5792c240a5
Sakal1009 <verasdone6@gmail.com>
2024-12-24 05:45:16 +0900

    dfdfdfdsfds

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-4">
       <div className="flex justify-end">
-        <Link to="https://proofofme.fun">
+        <Link href="https://proofofme.fun">
           <Button
             variant={'ghost'}
             size={'icon'}
