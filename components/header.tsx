c9df386f6f3674851495dec18868de9a44b3e0cb
Sakal1009 <verasdone6@gmail.com>
2024-12-24 05:31:37 +0900

    fdd

--- components/header.tsx
+++ components/header.tsx
@@ -7,14 +7,15 @@ import HistoryContainer from './history-container'
 
 export const Header: React.FC = async () => {
   const { theme } = useTheme()
+  console.log(theme)
   return (
     <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
         <a href="/">
-          {/* <IconLogo className={cn('w-5 h-5')} /> */}
-          {
+          <IconLogo className={cn('w-5 h-5')} />
+          {/* {
             theme === 'light' ? (<img src="/Logo-black.png" className={cn('w-10 h-10')} />) : (<img src="/Logo-white.png" className={cn('w-10 h-10')} />)
-          }
+          } */}
 
           <span className="sr-only">Proof of Me</span>
         </a>
