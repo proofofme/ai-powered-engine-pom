8fb7fc7185dd99cfaa5c67be65fcf51c750d40aa
Sakal1009 <verasdone6@gmail.com>
2024-12-24 05:37:17 +0900

    dfdfd

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,9 +5,9 @@ import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
+    <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-4">
       <div className="flex justify-end">
-        <Link href="https://proofofme.fun" passHref target="_blank">
+        <Link to="https://proofofme.fun">
           <Button
             variant={'ghost'}
             size={'icon'}
