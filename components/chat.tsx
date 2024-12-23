1fbc040b8674e9cadbe9b5e18c42ecd9806e4435
Sakal1009 <verasdone6@gmail.com>
2024-12-24 06:29:22 +0900

    hjhjhjhgjh

--- components/chat.tsx
+++ components/chat.tsx
@@ -24,7 +24,7 @@ export function Chat({ id, query }: ChatProps) {
   return (
     <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4">
       <div className="w-full flex flex-col gap-2 justify-center items-center">
-        <img src="/Logo-black.png" className="w-120" />
+        <img src="/Logo-black.png" className="w-20" />
         <span className="text-xl">Proof of Me</span>
       </div>
       <ChatMessages messages={messages} />
