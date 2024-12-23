a4505d7e8fa76ca874858aad20af2aaaacbcd1ea
Sakal1009 <verasdone6@gmail.com>
2024-12-24 06:19:41 +0900

    dfdfdf

--- components/chat.tsx
+++ components/chat.tsx
@@ -23,6 +23,10 @@ export function Chat({ id, query }: ChatProps) {
 
   return (
     <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4">
+      <div className="w-full flex flex-col gap-2 justify-center items-center">
+        <img src="/Logo-black.png" className="w-120" />
+        <span className="text-xl">Proof of Me</span>
+      </div>
       <ChatMessages messages={messages} />
       <ChatPanel messages={messages} query={query} />
     </div>
--- components/header.tsx
+++ components/header.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
-import { useTheme } from 'next-themes'
 import { ModeToggle } from './mode-toggle'
 import { IconLogo } from './ui/icons'
 import { cn } from '@/lib/utils'
 import HistoryContainer from './history-container'
 
-export const Header: React.FC = () => {
-  const { theme } = useTheme()
-  console.log(theme)
+export const Header: React.FC = async () => {
   return (
     <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
-        <a href="/">
-          {/* <IconLogo className={cn('w-5 h-5')} /> */}
-          {
-            theme === 'light' ? (<img src="/Logo-black.png" className={cn('w-10 h-10')} />) : (<img src="/Logo-white.png" className={cn('w-10 h-10')} />)
-          }
-
-          <span className="sr-only">Proof of Me</span>
-        </a>
+        {/* <a href="/"> */}
+        {/* <IconLogo className={cn('w-5 h-5')} /> */}
+        {/* <img src="/Logo-black.png" className={cn('w-10 h-10')} />
+          <span className="sr-only">Proof of Me</span> */}
+        {/* </a> */}
       </div>
       <div className="flex gap-0.5">
         <ModeToggle />
